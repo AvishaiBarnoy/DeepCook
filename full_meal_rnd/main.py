03aefdd7fef54192b3f862e89d80ccf5894b2568
avishai231 <avishai231@gmail.com>
2022-09-06 12:19:25 +0300

    basic changes for meal planner

--- full_meal_rnd/main.py
+++ full_meal_rnd/main.py
@@ -5,13 +5,10 @@ Here happens all the magic
 import pandas as pd
 #import scripts.auxillary as aux
 #import scripts.iodata as iod
-import typer
-import os
 from pathlib import Path
 
 # default absolute pathway
 MEAL_LIST = "data/meal_list.csv"
-#absolute_path = os.path.join(os.path.dirname(__file__), f"../data/{MEAL_LIST}")
 absolute_path = Path(__file__).parent / MEAL_LIST
 soup_list = ''
 main_courses = ''
@@ -19,9 +16,5 @@ side_dishes = ''
 salads = ''
 desserts = ''
 
-def main(soups: str = soup_list, main: str = main_courses, side: str = side_dishes, salad: str = salads,
-        dessert: str = desserts):
-    print("Not implemented yet")
+st.selectbox("Number of diners",tuple(range(1,11)))
 
-if __name__ == "__main__":
-    typer.run(main)
